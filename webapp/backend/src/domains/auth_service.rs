070d10658e2974ac34d242977543afb4bf4cdb19
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-07-27 13:05:32 +0900

    save

--- webapp/backend/src/domains/auth_service.rs
+++ webapp/backend/src/domains/auth_service.rs
@@ -2,6 +2,7 @@ use std::path::{Path, PathBuf};
 use std::process::Command;
 
 use actix_web::web::Bytes;
+use log::error;
 
 use crate::errors::AppError;
 use crate::models::user::{Dispatcher, Session, User};
